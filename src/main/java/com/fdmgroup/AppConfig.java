159b2cb9e89fde931e33b0bcea4100aa7bd10001
joshua sanda <joshua.sanda@fdmgroup.com>
2021-03-15 16:48:13 -0400

    Added Question class, Question controller and add the Bean to appConfig

--- src/main/java/com/fdmgroup/AppConfig.java
+++ src/main/java/com/fdmgroup/AppConfig.java
@@ -3,8 +3,14 @@ package com.fdmgroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fdmgroup.model.Question;
+
+
 @Configuration
 public class AppConfig {
 
-
+	@Bean
+	public Question userDaoOld(){
+		return new Question(); 
+	}
 }
