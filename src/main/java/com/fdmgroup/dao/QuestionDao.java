fb39a90aee1d8cb1363a72bd7a56099fd5c2bdf2
joshua sanda <joshua.sanda@fdmgroup.com>
2021-03-16 15:20:44 +0000

    Update QuestionDao.java

--- src/main/java/com/fdmgroup/dao/QuestionDao.java
+++ src/main/java/com/fdmgroup/dao/QuestionDao.java
@@ -3,9 +3,7 @@ package com.fdmgroup.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import com.fdmgroup.model.Question;
-import com.fdmgroup.model.User;
 
 public interface QuestionDao extends CrudRepository<Question, Integer>{
-	
-}
 
+}
