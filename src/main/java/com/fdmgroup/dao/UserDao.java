f2f398ca708d81c2f070df2e67a102d0608c2095
cbuttras <59930768+cbuttras@users.noreply.github.com>
2021-03-16 10:31:35 -0400

    crud repo

--- src/main/java/com/fdmgroup/dao/UserDao.java
+++ src/main/java/com/fdmgroup/dao/UserDao.java
@@ -1,5 +1,9 @@
 package com.fdmgroup.dao;
 
-public class UserDao {
+import org.springframework.data.repository.CrudRepository;
 
+import com.fdmgroup.model.User;
+
+public interface UserDao extends CrudRepository<User, Integer>{
+	
 }
