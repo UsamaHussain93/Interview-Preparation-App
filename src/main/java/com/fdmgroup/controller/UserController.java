628018b4d74c48a2f43e0424ce3c27dbd3af3939
cbuttras <59930768+cbuttras@users.noreply.github.com>
2021-03-16 13:37:50 -0400

    idk

--- src/main/java/com/fdmgroup/controller/UserController.java
+++ src/main/java/com/fdmgroup/controller/UserController.java
@@ -1,25 +1,28 @@
-//package com.fdmgroup.controller;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.fdmgroup.dao.UserDao;
-//import com.fdmgroup.model.User;
-//
-//@RestController
-//public class UserController {
-//	@Autowired
-//	UserDao userDao;
-//	
-//	@GetMapping("/users")
-//	public List<User> getAllUsers(){
-//		List<User> foundUsers = new ArrayList<>();
-//		userDao.findAll().forEach(user -> foundUsers.add(user));
-//		return foundUsers;
-//	}
-//	
-//}
+package com.fdmgroup.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fdmgroup.dao.UserDao;
+import com.fdmgroup.model.User;
+
+@RestController
+public class UserController {
+	@Autowired
+	UserDao userDao;
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		List<User> foundUsers = new ArrayList<>();
+		userDao.findAll().forEach(user -> foundUsers.add(user));
+		return foundUsers;
+	}
+	@GetMapping("/users/{id}")
+	public User getUser(int id) {
+		return userDao.findById(id).orElse(null);
+	}
+}
