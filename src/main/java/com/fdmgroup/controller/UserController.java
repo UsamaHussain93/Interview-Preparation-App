9b592fbe85f3f38efb0cf1ae05bc1e9e0762e16e
usama.hussain <usama.hussain@fdmgroup.com>
2021-03-18 08:32:16 -0600

    Test

--- src/main/java/com/fdmgroup/controller/UserController.java
+++ src/main/java/com/fdmgroup/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.fdmgroup.model.Question;
 import com.fdmgroup.model.User;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	@Autowired
 	UserDao userDao;
